03ee9be9327bc364d7c7e1b21aaa4ef9caec062d
tobiloba ogundiyan <apprentice_py@icloud.com>
2025-03-31 18:04:15 +0100

    100% coverage for config

--- config/cfg_test.go
+++ config/cfg_test.go
@@ -7,15 +7,13 @@ import (
 	"testing"
 )
 
-func TestLoadConfig(t *testing.T) {
-	tempfile := filepath.Join(t.TempDir(), "config.toml")
-	data := `
+var (
+	validConfig = `
 [smtp]
 server = "smtp.icloud.com"
 port = 587
 email = "apprentice_py@icloud.com"
 password = "12345"
-
 
 [api]
 apikey = "12345"
@@ -23,10 +21,9 @@ leagues = [11, 22, 33]
 teams = [11, 22, 33]
 timezone = "Europe/London"
 `
-	if err := os.WriteFile(tempfile, []byte(data), 0644); err != nil {
-		t.Fatalf("cannot write to file: %v", err)
-	}
-	want := &Config{
+	invalidConfig = "invalid config"
+
+	want = &Config{
 		Api: Apifutbol{
 			Apikey:   "12345",
 			Teams:    []int{11, 22, 33},
@@ -40,12 +37,46 @@ timezone = "Europe/London"
 			Password: "12345",
 		},
 	}
-	got, err := LoadConfig(tempfile)
-	if err != nil {
-		t.Fatalf("Cannot load config: %v", err)
+)
+
+func TestLoadConfig(t *testing.T) {
+	tests := map[string]struct {
+		data        string
+		want        *Config
+		expectError bool
+	}{
+		"valid config": {data: validConfig, want: want, expectError: false},
+		"No file":      {data: "", want: want, expectError: true},
+		"Invalid toml": {data: invalidConfig, want: want, expectError: true},
 	}
 
-	if !reflect.DeepEqual(*got, *want) {
-		t.Fatalf("got %v want %v", got, want)
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			tempfile := filepath.Join(t.TempDir(), "config.toml")
+
+			if tc.data != "" {
+				if err := os.WriteFile(tempfile, []byte(tc.data), 0644); err != nil {
+					t.Fatalf("cannot write to file: %v", err)
+				}
+			}
+
+			got, err := LoadConfig(tempfile)
+
+			if tc.expectError {
+				if err == nil {
+					t.Fatalf("expected an error but got nil")
+				}
+			} else {
+				if err != nil {
+					t.Fatalf("did not expect error, got: %v", err)
+				}
+
+				if !reflect.DeepEqual(*got, *tc.want) {
+					t.Fatalf("got %v want %v", got, tc.want)
+				}
+
+			}
+
+		})
 	}
 }
