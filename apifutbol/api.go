5967c8af6ab3fa506113aec8fe35531e900b262c
tobiloba ogundiyan <apprentice_py@icloud.com>
2025-04-08 12:13:45 +0100

    refactored fixture request

--- apifutbol/api.go
+++ apifutbol/api.go
@@ -9,6 +9,10 @@ import (
 )
 
 type (
+	APIResponse struct {
+		Response []*FixturesResponse `json:"response"`
+	}
+
 	FixturesResponse struct {
 		Fixture Fixture `json:"fixture"`
 		League  League  `json:"league"`
@@ -96,38 +100,39 @@ type (
 	}
 )
 
-func GetFixtures(timezone, apikey, date string) (*http.Response, error) {
+func GetFixtures(timezone, apikey, date string) ([]*FixturesResponse, error) {
 	client := &http.Client{}
-	params := url.Values{}
-	params.Add("date", date)
-	params.Add("timezone", timezone)
-	finalUrl := fmt.Sprintf("https://v3.football.api-sports.io/fixtures?%s", params.Encode())
+	url := buildFixturesUrl(timezone, date)
 
-	req, err := http.NewRequest("GET", finalUrl, nil)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Add("x-rapidapi-host", "v3.football.api-sports.io")
 	req.Header.Add("x-rapidapi-key", apikey)
+	req.Header.Add("Accept", "application/json")
 	resp, err := client.Do(req)
+	defer resp.Body.Close()
+
 	if err != nil {
 		return nil, err
 	}
-
-	return resp, nil
-}
-
-func DecodeResponse(resp *http.Response) ([]*FixturesResponse, error) {
-	defer resp.Body.Close()
-
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
-	var fixturesresp []*FixturesResponse
-	if err = json.Unmarshal(body, &fixturesresp); err != nil {
+	var wrapper APIResponse
+	if err = json.Unmarshal(body, &wrapper); err != nil {
 		return nil, err
 	}
-	return fixturesresp, nil
+	return wrapper.Response, nil
+
+}
+
+func buildFixturesUrl(timezone, date string) string {
+	params := url.Values{}
+	params.Add("date", date)
+	params.Add("timezone", timezone)
+	return fmt.Sprintf("https://v3.football.api-sports.io/fixtures?%s", params.Encode())
 }
