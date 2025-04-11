89ac2528e3c0df0e548d03720fdf4f8f265263bf
tobiloba ogundiyan <apprentice_py@icloud.com>
2025-04-11 11:42:33 +0100

    main done personal use now

--- main.go
+++ main.go
@@ -4,9 +4,83 @@ import (
 	"fmt"
 	"github.com/ogundiyantobiloba/emailfutbal/apifutbol"
 	"github.com/ogundiyantobiloba/emailfutbal/config"
+	"github.com/ogundiyantobiloba/emailfutbal/mailer"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 )
+
+type MatchCollector struct {
+	selection []string
+	seen      map[string]bool
+}
+
+func newMatchCollector() *MatchCollector {
+	return &MatchCollector{
+		selection: []string{},
+		seen:      make(map[string]bool),
+	}
+}
+
+func (m *MatchCollector) addMatches(fixture *apifutbol.FixturesResponse) {
+	layout := time.RFC3339
+	parsedTime, err := time.Parse(layout, fixture.Fixture.Date)
+	if err != nil {
+		log.Printf("error parsing time: %v", err)
+		return
+	}
+
+	formattedTime := parsedTime.Format("Mon 02 Jan, 3:04 PM")
+	match := fmt.Sprintf("%s vs %s - %s", fixture.Teams.Home.Name, fixture.Teams.Away.Name, formattedTime)
+
+	if !m.seen[match] {
+		m.selection = append(m.selection, match)
+		m.seen[match] = true
+	}
+}
+
+func (m *MatchCollector) Results() []string {
+	return m.selection
+}
+
+func selectFixtureByTeams(cfg *config.Config, fixtures []*apifutbol.FixturesResponse, mc *MatchCollector) {
+	for _, team := range cfg.Api.Teams {
+		for _, fixture := range fixtures {
+			if team == fixture.Teams.Home.Id || team == fixture.Teams.Away.Id {
+				mc.addMatches(fixture)
+			}
+		}
+	}
+}
+
+func selectFixtureByLeagues(cfg *config.Config, fixtures []*apifutbol.FixturesResponse, mc *MatchCollector) {
+	var Empty struct{}
+	leagues := make(map[int]struct{})
+
+	for _, leagueID := range cfg.Api.Leagues {
+		leagues[leagueID] = Empty
+	}
+	for _, fixture := range fixtures {
+		if _, ok := leagues[fixture.League.Id]; ok {
+			mc.addMatches(fixture)
+		}
+
+	}
+}
+
+func sendMail(mc *MatchCollector, cfg *config.Config) error {
+
+	m := mailer.NewMailer(cfg.Smtp.Server, cfg.Smtp.Port, cfg.Smtp.Email, cfg.Smtp.Password)
+	m.Recipient = cfg.Smtp.Email // For now personal need
+	m.Subject = "Tomorrow's Fixtures"
+	body := strings.Join(mc.Results(), "\n\n")
+	m.Body = body
+	if err := m.SendEmail(); err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	cfg, err := config.LoadConfig("config.toml")
@@ -21,36 +95,19 @@ func main() {
 
 	client := &http.Client{}
 	api := apifutbol.NewAPIClient(cfg.Api.Apiurl, cfg.Api.Apikey, date, cfg.Api.Timezone, client)
+	mc := newMatchCollector()
 	fixtures, err := api.GetFixtures()
-	selection := selectFixtureByTeams(cfg, fixtures)
-	fmt.Println(selection)
+	selectFixtureByTeams(cfg, fixtures, mc)
+	selectFixtureByLeagues(cfg, fixtures, mc)
 
-}
-
-func selectFixtureByTeams(cfg *config.Config, fixtures []*apifutbol.FixturesResponse) []string {
-	var selection []string
-	seen := make(map[string]bool)
-
-	for _, team := range cfg.Api.Teams {
-		for _, f := range fixtures {
-			fixture := f.Teams
-
-			if team == fixture.Home.Id || team == fixture.Away.Id {
-				key := fmt.Sprintf("%s-vs-%s-%s", fixture.Home.Id, fixture.Away.Id, f.Fixture.Timestamp)
-
-				if !seen[key] {
-					match := fmt.Sprintf("%s vs %s - %s", fixture.Home.Name, fixture.Away.Name, f.Fixture.Date)
-					selection = append(selection, match)
-					seen[key] = true
-				}
-			}
-		}
+	if len(mc.Results()) == 0 {
+		log.Println("No matches found for tomorrow. No email sent.")
+		return
 	}
 
-	return selection
-}
-
-func selectFixtureByLeagues(cfg *config.Config, fixtures []*apifutbol.FixturesResponse) []string {
-
-	return []string{""}
+	if err = sendMail(mc, cfg); err != nil {
+		log.Fatalf("Failed to send email: %v", err)
+	} else {
+		fmt.Println("Email sent successfully")
+	}
 }
